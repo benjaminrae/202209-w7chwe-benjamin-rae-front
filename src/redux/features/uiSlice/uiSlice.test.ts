77af4091e7cefd0517d9c914408395178ac886df
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-11-12 20:21:12 +0100

    Add tests for modal actions

--- src/redux/features/uiSlice/uiSlice.test.ts
+++ src/redux/features/uiSlice/uiSlice.test.ts
@@ -2,7 +2,9 @@ import mockUiState from "../../../mocks/states/mockUiState";
 import { UiState } from "./types";
 import {
   hideLoadingActionCreator,
+  hideModalActionCreator,
   showLoadingActionCreator,
+  showModalActionCreator,
   uiReducer,
 } from "./uiSlice";
 
@@ -48,6 +50,48 @@ describe("Given a uiReducer", () => {
           initialUiState,
           hideLoadingActionCreator()
         );
+
+        expect(newUiState).toStrictEqual(expectedUiState);
+      });
+    });
+
+    describe("and it receives the initial ui state and a show modal action creator with payload isError true and text 'There was an error'", () => {
+      test("Then it should return a copy of the initial state with showModal true, isError true and modalText 'There was an error'", () => {
+        const actionPayload = {
+          isError: true,
+          modalText: "There was an error",
+        };
+        const initialUiState: UiState = {
+          ...mockUiState,
+        };
+        const expectedUiState: UiState = {
+          ...mockUiState,
+          showModal: true,
+          modalText: actionPayload.modalText,
+          isError: actionPayload.isError,
+        };
+
+        const newUiState = uiReducer(
+          initialUiState,
+          showModalActionCreator(actionPayload)
+        );
+
+        expect(newUiState).toStrictEqual(expectedUiState);
+      });
+    });
+
+    describe("and it receives an initial state with showModal true and a hideModal action creator", () => {
+      test("Then it should return a copy of the initial state with showModal false", () => {
+        const initialUiState: UiState = {
+          ...mockUiState,
+          showModal: true,
+        };
+        const expectedUiState: UiState = {
+          ...mockUiState,
+          showModal: false,
+        };
+
+        const newUiState = uiReducer(initialUiState, hideModalActionCreator());
 
         expect(newUiState).toStrictEqual(expectedUiState);
       });
