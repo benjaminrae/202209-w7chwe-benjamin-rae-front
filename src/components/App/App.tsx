8a95effc7aa80db938c1ed1a7a0879cae42fa252
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-11-12 16:45:33 +0100

    Create button component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,13 @@
+import Button from "../Button/Button";
 import AppStyled from "./AppStyled";
 
 const App = (): JSX.Element => {
-  return <AppStyled className="app">Feisbuk</AppStyled>;
+  return (
+    <AppStyled className="app">
+      Feisbuk
+      <Button text="Sign up" />
+    </AppStyled>
+  );
 };
 
 export default App;
---
+++ src/components/Button/ButtonStyled.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+import sizes from "../../styles/sizes";
+
+const ButtonStyled = styled.button`
+  color: ${(props) => props.theme.backgroundColor};
+  background-color: ${(props) => props.theme.colorPrimary};
+  border: 2px solid ${(props) => props.theme.colorPrimary};
+  border-radius: 0.5rem;
+  padding: ${sizes.button.padding};
+  &:hover,
+  :focus {
+    color: ${(props) => props.theme.colorPrimary};
+    background-color: ${(props) => props.theme.backgroundColor};
+  }
+`;
+
+export default ButtonStyled;
